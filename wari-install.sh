f18afbe4e96b7fb57f913f098248bb5c9d89b967
Stuart Campbell <stuart@stuartcampbell.me>
2016-01-19 07:54:51 -0500

    Explicitly set WARI_ROOT, add more repos

--- wari-install.sh
+++ wari-install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-. $HOME/code/wari/wari-setup.sh
+export WARI_ROOT=$HOME/code/wari 
+
+. $WARI_ROOT/wari-setup.sh
 
 add_repo spotify 
 add_repo epel
@@ -8,10 +10,15 @@ add_repo sns
 add_repo sic-science
 add_repo sic-backports-el7
 
+add_repo chrome
+
 # Some themes
 add_repo arc-theme
 add_repo moka
 add_repo numix
+add_repo paper
+
+add_repo nvidia
 
 add_packages_from_distro_file
  
